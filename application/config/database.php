9c04ded9bfe4be742fb030101fd99304293e437d
Rick Daniel <rick@araishikeiwai.com>
2014-10-29 18:14:49 +0700

    Config database

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'ppshop';
+$db['default']['password'] = 'ppshop';
 $db['default']['database'] = 'ppshop';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
